f0aaac39ac5611359540577cb26da43e6b0d56e0
yuri-hyouka <ti.yuri.p@gmail.com>
2025-06-23 19:47:37 -0300

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+
+resource "aws_instance" "app_server" {
+  subnet_id     = aws_subnet.main.id   
+  ami           = "ami-0779caf41f9ba54f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Primeira instancia"
+  }
+}
