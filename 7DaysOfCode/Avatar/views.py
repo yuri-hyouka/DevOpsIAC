52b325b21dc52c0938b5fb5b3d03c7ac9b5c8a96
yuri-hyouka <ti.yuri.p@gmail.com>
2025-07-07 22:12:28 -0300

    7/7 Desafio concluido

--- 7DaysOfCode/Avatar/views.py
+++ 7DaysOfCode/Avatar/views.py
@@ -1,25 +1,51 @@
 from django.shortcuts import render
 import requests
-import json
 from googletrans import Translator
 
 def getTraducao(texto):
     translator = Translator()
-    resultado  = translator.translate(texto,dest='en')
+    resultado = translator.translate(texto, dest='pt')
     return resultado.text
 
 def personagens(request):
-    api_url  = f'https://last-airbender-api.fly.dev/api/v1/characters'
+    pagina = int(request.GET.get('page', 1))
+    limite = 10
+    offset = (pagina - 1) * limite
+
+    api_url = f'https://last-airbender-api.fly.dev/api/v1/characters?perPage={limite}&page={pagina}'
     response = requests.get(api_url)
-    dados    = response.json()
+
+    if response.status_code != 200:
+        return render(request, 'Avatar/lista_traduzida.html', {
+            'personagens': [],
+            'pagina': pagina,
+            'tem_mais': False,
+            'erro': 'Erro ao acessar a API externa.'
+        })
+
+    dados = response.json()
     lista_traduzida = []
 
     for item in dados:
-        nome      = item.get('name', 'Desconhecido')
+        nome = item.get('name', 'Desconhecido')
         afiliacao = item.get('affiliation', 'Sem afiliação')
+        foto = item.get('photoUrl')
         lista_traduzida.append({
             'nome': getTraducao(nome),
             'afiliacao': getTraducao(afiliacao),
+            'foto': foto
         })
 
-    return render(request, 'Avatar/lista_traduzida.html', {'personagens': lista_traduzida})
+    # Verifica se há mais personagens na próxima página
+    tem_mais = len(dados) == limite
+
+    # Número de páginas visíveis
+    total_paginas = list(range(1, pagina + 3))
+
+    return render(request, 'Avatar/lista_traduzida.html', {
+        'personagens': lista_traduzida,
+        'pagina': pagina,
+        'tem_mais': tem_mais,
+        'offset': offset,
+        'total_paginas': total_paginas
+    })
